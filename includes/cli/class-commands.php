94efb9ba78fa69770c050b9d5d4f7b50f0a48d7f
sultann <manikdrmc@gmail.com>
2017-10-30 23:56:44 +0600

    add type specific metas

--- includes/cli/class-commands.php
+++ includes/cli/class-commands.php
@@ -9,20 +9,21 @@ class WPCP_Seeder_Command extends WP_CLI_Command {
      *
      * ## EXAMPLES
      *
-     * wp erp-seeder seed customer --num=2000
+     * wp wpcp-seeder seed random --num=10
      *
      * @synopsis <type> [--num=<number>]
      */
     public function seed( $args, $assoc_args ) {
-        list( $name ) = $args;
-        $type         = strtolower( $name );
-        $number       = isset( $assoc_args['num'] ) ? $assoc_args['num'] : 0;
+	    list( $name ) = $args;
+	    $type         = strtolower( $name );
+	    $number       = isset( $assoc_args['num'] ) ? $assoc_args['num'] : 0;
 
-        for ( $i = 1 ; $i <= $number ; $i++ ){
+	    $faker = Faker\Factory::create('en_US');
+	    for ( $i = 1 ; $i <= $number ; $i++ ){
 
 	        $post_id = wp_insert_post(
 		        array(
-			        'post_title' => 'Campaign '. $i,
+			        'post_title' => $faker->sentence(4, true),
 			        'post_status' => 'publish',
 			        'post_type' => 'wp_content_pilot',
 		        )
@@ -30,22 +31,34 @@ class WPCP_Seeder_Command extends WP_CLI_Command {
 
 	        if( !$post_id  ) continue;
 
-	        $faker = Faker\Factory::create();
-			$campaign_type = $faker->randomElements(array_keys(wpcp_get_campaign_types()));
+	        $campaign_type = $faker->randomElements(array_keys(wpcp_get_campaign_types()));
 	        update_post_meta( $post_id, '_wpcp_campaign_type', $campaign_type[0]);
-	        update_post_meta( $post_id, '_wpcp_active', 'on');
+	        update_post_meta( $post_id, '_wpcp_active', $faker->numberBetween(0, 1));
 	        update_post_meta( $post_id, '_wpcp_campaign_target', $faker->numberBetween($min = 1, $max = 5));
+	        update_post_meta( $post_id, '_wpcp_frequency', $faker->numberBetween($min = 1, $max = 5));
 	        update_post_meta( $post_id, '_wpcp_frequency_period', $faker->randomElements(['hour', 'day'])[0]);
 	        update_post_meta( $post_id, '_wpcp_content_type', $faker->randomElements(['html', 'text'])[0]);
 
-	        update_post_meta( $post_id, '_wpcp_first_image_featured', $faker->randomElements(['on', 'no'])[0]);
-	        update_post_meta( $post_id, '_wpcp_skip_if_no_image', $faker->randomElements(['on', 'no'])[0]);
-	        update_post_meta( $post_id, '_wpcp_remove_all_image', $faker->randomElements(['on', 'no'])[0]);
-	        update_post_meta( $post_id, '_wpcp_skip_if_not_eng', $faker->randomElements(['on', 'no'])[0]);
-	        update_post_meta( $post_id, '_wpcp_post_summary', $faker->randomElements(['on', 'no'])[0]);
-	        update_post_meta( $post_id, '_wpcp_strip_links', $faker->randomElements(['on', 'no'])[0]);
+	        update_post_meta( $post_id, '_wpcp_first_image_featured', $faker->numberBetween(0, 1));
+	        update_post_meta( $post_id, '_wpcp_skip_if_no_image', $faker->numberBetween(0, 1));
+	        update_post_meta( $post_id, '_wpcp_remove_all_image', $faker->numberBetween(0, 1));
+	        update_post_meta( $post_id, '_wpcp_skip_if_not_eng', $faker->numberBetween(0, 1));
+	        update_post_meta( $post_id, '_wpcp_post_summary', $faker->numberBetween(0, 1));
+	        update_post_meta( $post_id, '_wpcp_strip_links', $faker->numberBetween(0, 1));
 
 
+	        update_post_meta( $post_id, '_wpcp_last_run', $faker->unixTime($max = 'now'));
+
+
+	        //post settings
+		    update_post_meta( $post_id, '_wpcp_post_type', $faker->randomElement(array_keys(wpcp_supported_get_post_types()))[0]);
+		    update_post_meta( $post_id, '_wpcp_post_author', $faker->randomElement(array_keys(wpcp_get_authors()))[0]);
+
+	        //advance
+		    update_post_meta( $post_id, '_wpcp_title_limit', $faker->numberBetween($min = 40, $max = 80));
+		    update_post_meta( $post_id, '_wpcp_content_limit', $faker->numberBetween($min = 500, $max = 2000));
+
+			$this->populate_campaign_specific_contents( $post_id, $campaign_type[0]);
 
 
 
@@ -59,6 +72,73 @@ class WPCP_Seeder_Command extends WP_CLI_Command {
         WP_CLI::success( $number );
         WP_CLI::success( "Tables are successfully seeded!" );
     }
+
+
+    function populate_campaign_specific_contents ($post_id, $type) {
+	    $faker = Faker\Factory::create('en_US');
+	    $size = $faker->numberBetween($min = 5, $max = 10);
+
+	    switch ($type){
+		    case 'feeds':
+			    $feed_links = [];
+			    for ($i=1; $i <= $size; $i++ ){
+				    $link = $faker->url;
+				    $parsed_url = parse_url($link);
+				    $feed_links[] = untrailingslashit(esc_url_raw($parsed_url['host']));
+			    }
+
+			    $string_feed_links = implode(PHP_EOL, $feed_links);
+			    update_post_meta( $post_id, '_wpcp_feed_links', $string_feed_links);
+				//@todo add premimum data
+
+			    break;
+		    case 'articles':
+			    $words = [];
+			    for ($i=1; $i <= $size; $i++ ){
+				    $words[] = str_replace('.', '', sanitize_title($faker->realText($maxNbChars = 20, $indexSize = 1)));
+			    }
+			    $keywords = implode(PHP_EOL, $words);
+			    update_post_meta( $post_id, '_wpcp_keywords', $keywords);
+			    update_post_meta( $post_id, '_wpcp_article_source', $faker->randomElement(array_keys(wpcp_get_article_sources()))[0]);
+
+			    break;
+		    case 'youtube':
+			    $words = [];
+			    for ($i=1; $i <= $size; $i++ ){
+				    $words[] = str_replace('.', '', sanitize_title($faker->realText($maxNbChars = 20, $indexSize = 1)));
+			    }
+			    $keywords = implode(PHP_EOL, $words);
+			    update_post_meta( $post_id, '_wpcp_keywords', $keywords);
+			    break;
+		    case 'flicker':
+			    $words = [];
+			    for ($i=1; $i <= $size; $i++ ){
+				    $words[] = str_replace('.', '', sanitize_title($faker->realText($maxNbChars = 20, $indexSize = 1)));
+			    }
+			    $keywords = implode(PHP_EOL, $words);
+			    update_post_meta( $post_id, '_wpcp_keywords', $keywords);
+			    break;
+
+
+
+
+
+
+
+
+		    	break;
+
+
+
+		    default:
+
+			    break;
+	    }
+
+
+    }
+
+
 }
 
 WP_CLI::add_command( 'wpcp-seeder', 'WPCP_Seeder_Command' );
